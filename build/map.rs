7a237fb1b1c6d829f25a6a9c710be3fe9c578483
mccarrion <25594552+mccarrion@users.noreply.github.com>
2022-08-02 23:25:31 -0700

    refactor to map tile gid to tile png

--- build/map.rs
+++ build/map.rs
@@ -2,6 +2,9 @@ use std::borrow::Borrow;
 use std::collections::HashMap;
 use std::fs;
 use std::fs::DirEntry;
+use image::{DynamicImage, GenericImage};
+use image::imageops::tile;
+use image::math::Rect;
 use serde::*;
 use serde_json::*;
 
@@ -20,13 +23,13 @@ struct TileSet {
     tilewidth: i16,
     #[serde(alias = "type")]
     ts_type: String,
-    version: String
+    version: String,
 }
 
 #[derive(Serialize, Deserialize)]
 struct TileSetId {
     firstgid: i16,
-    source: String
+    source: String,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -34,19 +37,19 @@ struct TileMap {
     compressionlevel: i16,
     height: i16,
     infinite: bool,
-    layers: Vec<TileLayer>, // update to be list of structs
+    layers: Vec<TileLayer>,
     nextlayerid: i16,
     nextobjectid: i16,
     orientation: String,
     renderorder: String,
     tiledversion: String,
     tileheight: i16,
-    tilesets: Vec<TileSetId>, // update to be list of structs
+    tilesets: Vec<TileSetId>,
     tilewidth: i16,
     #[serde(alias = "type")]
     tm_type: String,
     version: String,
-    width: i16
+    width: i16,
 }
 
 #[derive(Serialize, Deserialize)]
@@ -69,26 +72,45 @@ pub fn generate_map_from_tiled_config() {
     let mut tilemap: TileMap = from_str(&tilemap_data.unwrap()).unwrap();
     let tileset_ids: Vec<TileSetId> = tilemap.tilesets;
 
-    /*
-     * This code generates two maps based on Tiled config
-     *      tileset_map: this is a map of the firstgid of a TileSet config to a defined TileSet
-     *      range_cache_map: this is a map of each id for a TileSet to the firstgid in a TileSet
-     *
-     * These two maps are sufficient to begin drawing PNG files of map layers based on Tiled config
-     */
-    let mut tileset_map: HashMap<i16, TileSet> = HashMap::new();
-    let mut range_cache_map: HashMap<i16, i16> = HashMap::new();
+    let mut tile_map: HashMap<i16, DynamicImage> = HashMap::new();
     for tileset_id in tileset_ids {
         let str: String = String::from(&tileset_id.source);
-        let tileset_dir: String = str.replace("..", "../assets/tiled");
-        let tileset_data: String = fs::read_to_string(tileset_dir).unwrap();
+        let tileset_json_dir: String = str.replace("..", "../assets/tiled");
+        let tileset_data: String = fs::read_to_string(tileset_json_dir).unwrap();
         let tileset: TileSet = from_str(&tileset_data).unwrap();
-        let lastgid = tileset_id.firstgid + tileset.tilecount;
-        for n in tileset_id.firstgid..lastgid {
-            range_cache_map.insert(n,tileset_id.firstgid);
+        let dir: String = String::from(&tileset.image);
+        let tileset_img_dir: String = dir.replace("../..", "../assets");
+        let mut img: DynamicImage = image::open(tileset_img_dir).unwrap();
+        let tile_quantity: i16 = tileset.tilecount;
+        let mut col: i16 = 0;
+        let mut row: i16 = 0;
+        let mut tile_id = tileset_id.firstgid;
+        let columns: i16 = tileset.columns;
+        /*
+         * This loop maps all tiles to their gid assigned by Tiled
+         * TODO: this loop has an off-by-one bug need to fix
+         */
+        for n in 0..tile_quantity {
+            let x_offset: u32 = (tileset.tilewidth * col) as u32;
+            let y_offset: u32 = (tileset.tileheight * row) as u32;
+            let tile_img = img.crop(
+                x_offset,
+                y_offset,
+                tileset.tilewidth as u32,
+                tileset.tilewidth as u32);
+            if n % columns == 0 {
+                col = 0;
+                row += 1;
+            }
+            tile_map.insert(tile_id, tile_img);
+            col += 1;
+            tile_id += 1;
         }
-        tileset_map.insert(tileset_id.firstgid, tileset);
     }
 
 
+    let layers: Vec<TileLayer> = tilemap.layers;
+    for layer in layers {
+        // TODO: create PNG layers here
+    }
 }
